2a8a76a4a1ad89f3b7f2597d11f07f8c0c5f5069
AntoKay <antonio.caiazzo.5490@gmail.com>
2024-07-07 12:27:17 +0200

    small changes

--- app/src/main/java/com/example/deck_check/MainActivity.kt
+++ app/src/main/java/com/example/deck_check/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.*
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
